83911d966f155cb47aafe608936aef73641fd016
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-10-03 04:45:12 +0000

    Add personType.h

---
+++ personType.h
@@ -0,0 +1,11 @@
+/*
+    Programmer: Ryan Posey
+    Assignment: L10.C
+    Purpose: Practice using constructors
+    Due date: 10/02/2025
+*/
+
+#ifndef PERSON_TYPE_H
+#define PERSON_TYPE_H
+
+#endif // PERSON_TYPE_H
