34d6b853c403c0d8c25a03808bbacdb1077facbc
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-10-03 05:36:52 +0000

    L10_C.cpp: merge calls to personType::setFirstName() & -setLastName() to -setName()

--- L10_C.cpp
+++ L10_C.cpp
@@ -21,8 +21,7 @@ int main() {
 
     // Init p1 attributesas garbage values
 
-    p1.setFirstName("");
-    p1.setLastName("");
+    p1.setName("");
     p1.setAddress("");
     p1.setHeight(0);
     p1.setDOB("");
@@ -31,29 +30,16 @@ int main() {
 
     // Reassign p1 attributes
 
-    p1.setFirstName("Sabrina");
-    p1.setLastName("Morales");
+    p1.setName("Sabrina Morales");
     p1.setAddress("662 S. Southhampton Avenue Vista, CA 92083");
     p1.setHeight(51.2);
     p1.setDOB("24 July 2017");
     p1.setGender('F');
     p1.setAge(20);
-    
-    cout << left << setw(10) << setfill('-') << "" << " # Test Getters # " << left << setw(10) << setfill('-') << "" << endl;
-
-    cout << setfill(' ');
-
-    cout <<  left << setw(20) << " | Name: " << left << p1.getFirstName() << " " << p1.getLastName() << endl
-        << left << setw(20) << " | Address: " << left << p1.getAddress() << endl
-        << left << setw(20) << " | Height: " << left << std::fixed << std::setprecision(1) << p1.getHeight() << " in" << endl
-        << left << setw(20) << " | DOB: " << left << p1.getDOB() << endl
-        << left << setw(20) << " | Gender: " << left << p1.getGender() << endl
-        << left << setw(20) << " | Age: " << left << static_cast<int>(p1.getAge()) << endl;
 
     personType p2{};
 
-    p2.setFirstName("Elle");
-    p2.setLastName("Macdonald");
+    p2.setName("Elle Macdonald");
     p2.setAddress("84 Green Drive Tracy, CA 95376");
     p2.setHeight(52.3);
     p2.setDOB("17 September 2014");
@@ -62,8 +48,7 @@ int main() {
 
     personType p3{};
 
-    p3.setFirstName("Hannah");
-    p3.setLastName("Finley");
+    p3.setName("Hannah Finley");
     p3.setAddress("8253 Linda Dr. Laguna Niguel, CA 92677");
     p3.setHeight(49.8);
     p3.setDOB("13 September 2012");
@@ -72,8 +57,7 @@ int main() {
 
     personType p4{};
 
-    p4.setFirstName("Arabella");
-    p4.setLastName("Walter");
+    p4.setName("Arabella Walter");
     p4.setAddress("7593 Wood St. Fountain Valley, CA 92708");
     p4.setHeight(54.1);
     p4.setDOB("27 April 2010");
@@ -82,8 +66,7 @@ int main() {
 
     personType p5{};
 
-    p5.setFirstName("Kye");
-    p5.setLastName("Baird");
+    p5.setName("Kye Baird");
     p5.setAddress("8316 Lafayette Ave. North Hills, CA 91343");
     p5.setHeight(55.8);
     p5.setDOB("28 November 2008");
