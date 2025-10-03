94b50b8590bc632a2dcdd8153a7f7fb5d07ecb18
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-10-03 04:46:29 +0000

    Add personType.cpp

---
+++ personType.cpp
@@ -0,0 +1,11 @@
+/*
+    Programmer: Ryan Posey
+    Assignment: L10.C
+    Purpose: Practice using constructors
+    Due date: 10/02/2025
+*/
+
+#include <iostream>
+#include "personType.h"
+
+using namespace std;
